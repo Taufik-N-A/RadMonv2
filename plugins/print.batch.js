a9237b948eb8304c6c2c5f2d414ff4de345b13b4
Maizil <142029402+Maizil41@users.noreply.github.com>
2024-11-18 19:43:12 +0700

    Fix encode url

--- plugins/print.batch.js
+++ plugins/print.batch.js
@@ -19,6 +19,6 @@ async function fetchBatchData(){try{document.getElementById("loading").style.dis
                 </div>
             `;container.appendChild(batchCard)});document.getElementById("loading").style.display="none";document.querySelector(".main-container").style.display="block"}catch(error){console.error("Error fetching data:",error)}}
 function openPrintWindow(planName,accountsStr,batchName){const selectedOption=document.getElementById("prinMode").value;const validOptions=["printTickets1.php","printTickets2.php","printTickets3.php","printTickets4.php"];if(!validOptions.includes(selectedOption)){alert("Pilihan tidak valid!");return}
-const url=`./${selectedOption}?type=batch&plan=${encodeURIComponent(planName)}&accounts=Username,Password||${encodeURIComponent(accountsStr)}`;const newWindow=window.open(url,"_blank","width=800,height=600,scrollbars=yes");newWindow.onload=function(){newWindow.print()}}
+const url=`./${selectedOption}?type=batch&plan=${planName}&accounts=Username,Password||${accountsStr}`;const newWindow=window.open(url,"_blank","width=800,height=600,scrollbars=yes");newWindow.onload=function(){newWindow.print()}}
 function deleteBatch(batchId,batchName){if(confirm("Apakah anda yakin ingin menghapus batch "+batchName+"?")){$.ajax({url:"../backend/delbatch.php",type:"GET",data:{id:batchId},success:function(){location.reload()},error:function(xhr,status,error){console.error("Terjadi kesalahan:",error);alert("Terjadi kesalahan saat menghapus batch")},})}}
 fetchBatchData()
